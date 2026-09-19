9d6d97e3d0b18ed061764c5deff8ff9ba410751e
Ryan Storm <43070937+rstor1@users.noreply.github.com>
2026-09-19 09:13:04 -0400

    Improve sticky and mobile navigation behavior for the reference design.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,106 +1,61 @@
-
 $(document).ready(function () {
+    var $nav = $('nav');
+    var $navToggle = $('.js--nav-icon');
+    var $mainNav = $('.js--main-nav');
 
-    /*Sticky navigation*/
+    /* Sticky navigation */
     $('.js--section-about').waypoint(function (direction) {
-        if (direction === "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky')
-        }
-
+        $nav.toggleClass('sticky', direction === 'down');
     }, {
-        offset: '60px;'
+        offset: '60px'
     });
 
+    function closeMobileNav() {
+        $mainNav.stop(true, true).slideUp(200).removeClass('show');
+        $navToggle.attr('aria-expanded', 'false');
+        $navToggle.find('i').removeClass('ion-android-close').addClass('ion-android-menu');
+    }
+
     function checkWidth() {
-        var $window1 = $(window);
-        var windowsize1 = $window1.width();
-        if (windowsize1 > 767) {
-            $('.main-nav').css("display", "inline-block");
+        if ($(window).width() > 767) {
+            $mainNav.removeAttr('style').removeClass('show');
+            $navToggle.attr('aria-expanded', 'false');
         } else {
-            $('.main-nav').css("display", "none");
-            $('.js--main-nav').slideUp();
-            $('.js--nav-icon i').addClass('ion-android-menu');
+            closeMobileNav();
         }
     }
 
-    /*Scroll on buttons*/
-
-    /*Navigation scroll*/
-    $(function () {
-        $('a[href*="#"]:not([href="#"])').click(function () {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    var _$window = $(window);
-                    var _windowsize = _$window.width();
-                    if (_windowsize < 767) { // smaller then slide up the menu and put menu back instead of X icon
-                        $('.js--main-nav').slideUp();
-                        $('.js--nav-icon i').addClass('ion-android-menu');
-                    }
-                    return false;
-                }
+    $('a[href*="#"]:not([href="#"])').on('click', function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({ scrollTop: target.offset().top }, 700);
+                if ($(window).width() < 768) closeMobileNav();
+                return false;
             }
-        });
-    });
-
-    /*Animations on scroll*/
-    $('.js--wp-1').waypoint(function (direction) {
-        $('.js--wp-1').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
-
-    $('.js--wp-2').waypoint(function (direction) {
-        $('.js--wp-2').addClass('animated fadeInUp');
-    }, {
-        offset: '50%'
-    });
-
-    $('.js--wp-3').waypoint(function (direction) {
-        $('.js--wp-3').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
-
-    $('.js--wp-4').waypoint(function (direction) {
-        $('.js--wp-4').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
-
-    $('.js--wp-5').waypoint(function (direction) {
-        $('.js--wp-5').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
-
-    $('.js--wp-6').waypoint(function (direction) {
-        $('.js--wp-6').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
-
-    /*Mobile navigation*/
-    $('.js--nav-icon').click(function () {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
-        nav.slideToggle(200);
-
-        if (icon.hasClass('ion-android-menu')) {
-            icon.addClass('ion-android-close');
-            icon.removeClass('ion-android-menu');
-        } else {
-            icon.addClass('ion-android-menu');
-            icon.removeClass('ion-android-close');
         }
     });
 
-    $(window).resize(checkWidth); // check width to help with navigation menu
+    $('.js--wp-1').waypoint(function () { $('.js--wp-1').addClass('animated fadeIn'); }, { offset: '50%' });
+    $('.js--wp-2').waypoint(function () { $('.js--wp-2').addClass('animated fadeInUp'); }, { offset: '50%' });
+    $('.js--wp-3').waypoint(function () { $('.js--wp-3').addClass('animated fadeIn'); }, { offset: '50%' });
+    $('.js--wp-4').waypoint(function () { $('.js--wp-4').addClass('animated fadeIn'); }, { offset: '50%' });
+    $('.js--wp-5').waypoint(function () { $('.js--wp-5').addClass('animated fadeIn'); }, { offset: '50%' });
+    $('.js--wp-6').waypoint(function () { $('.js--wp-6').addClass('animated fadeIn'); }, { offset: '50%' });
 
+    /* Mobile navigation */
+    $navToggle.on('click', function () {
+        var isOpen = $mainNav.hasClass('show');
+        if (isOpen) {
+            closeMobileNav();
+        } else {
+            $mainNav.stop(true, true).slideDown(200).addClass('show');
+            $navToggle.attr('aria-expanded', 'true');
+            $navToggle.find('i').removeClass('ion-android-menu').addClass('ion-android-close');
+        }
+    });
+
+    $(window).on('resize', checkWidth);
+    checkWidth();
 });
